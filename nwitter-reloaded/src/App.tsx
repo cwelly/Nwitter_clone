312a97b032b9915f74983c99af8b06db264ba834
cwelly <potato3884@naver.com>
2024-09-05 17:34:13 +0900

    update router

--- nwitter-reloaded/src/App.tsx
+++ nwitter-reloaded/src/App.tsx
@@ -1,9 +1,52 @@
- 
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Layout from "./components/layout"
+import Profile from "./routes/profile"
+import Home from "./routes/home"
+import Login from "./routes/login"
+import CreateAccount from "./routes/create-account"
+import { createGlobalStyle } from "styled-components"
+import reset from "styled-reset"
 
-function App() { 
+const GlobalStyles = createGlobalStyle`
+  ${reset};
+  *{
+    box-sizing: border-box;
+  }
+  body {
+  background-color : black;
+  color:white;
+  font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif
+  }
+ `;
+const router = createBrowserRouter([{
+  path: "/",
+  element: <Layout></Layout>,
+  children: [
+    {
+      path: "",
+      element: <Home />,
+    },
+    {
+      path: "profile",
+      element: <Profile />
+    }
+  ]
+},
+{
+  path: "/login",
+  element: <Login />,
+}, {
+  path: "/create-account",
+  element: <CreateAccount />,
+},
+])
+
+function App() {
 
   return (
-    <> 
+    <>
+      <GlobalStyles></GlobalStyles>
+      <RouterProvider router={router} />
     </>
   )
 }
